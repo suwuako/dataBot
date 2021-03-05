4626f43cab4e6d0e3d58c33b8c661427745ca120
Vincent <defspyvc41@yandex.com>
2021-03-05 08:19:41 -0500

    Update odstest.py to use local-data dir

--- odstest.py
+++ odstest.py
@@ -6,11 +6,14 @@
 import pyexcel_ods
 import collections
 
+# define local storage directory here
+local_dir="local-data/"
+
 entireSpreadsheet = collections.OrderedDict()
 entireSpreadsheet.update({"Server1Sheet":[[1, 2, 3, 4, 5, 6], [1, 1, 2, 3, 5, 8]]})
 entireSpreadsheet.update({"Server2Sheet":[["Username#pin", "message"],["chocorho#1337", "This is just a test message"]]})
 #entireSpreadsheet.update({"Server2Sheet":[["", "This is just a test message"]]})
 #entireSpreadsheet.update({"Server2Sheet":[["chocorho#1337", "This is just a test message"]]})
 
-pyexcel_ods.save_data("testSheetFromPython.ods", entireSpreadsheet)
+pyexcel_ods.save_data(local_dir+"testSheetFromPython.ods", entireSpreadsheet)
 
