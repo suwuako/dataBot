f20f7e90e9c76e303780ae22a01a7070dd6fb7a5
l1tty <43295199+megumin00@users.noreply.github.com>
2021-04-18 02:11:13 +0800

    Delete secret.py

--- secret.py
+++
@@ -1,7 +0,0 @@
-#bot_token is the bot token of your bot from the discord developers portal
-#update_channel_ID is for the channel ID of where the bot notifies you of anything important
-#host_username is for collaborative work so that people know who knows running the current version of the bot
-
-bot_token = 'bot-token-here'
-update_channel_ID = channel-id-here 
-host_username = '<@431491115775164418>'
