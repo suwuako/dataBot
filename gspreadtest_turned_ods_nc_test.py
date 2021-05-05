5157b74f93f63ba0dce1a9d21a48c30957fcad2e
Vincent <defspyvc41@yandex.com>
2021-05-05 17:15:34 -0400

    Begin rewriting gspreadtest, refining main.py
    This update will start a major rewrite of the core of gspreadtest, the main
    building-block of the spreadsheet operations. It will also start refining
    the code for the databot itself, which depends on gspreadtest.
    
    The new variant of "gspreadtest" is designed to follow the original
    specification as closely as possible (using the same method names whenever
    possible), but it will instead use ODS files and Nextcloud as opposed to
    Google Drive documents.
    
    Obviously, more work is needed, but the reader will notice that the basic test
    depicted in the new gspreadtest file should be functional.
    
    Let us be free and control our own data!

---
+++ gspreadtest_turned_ods_nc_test.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+"""
+    A consolidated class to unify the gspreadtest class and the
+    minimal_upload_example.
+    It serves as a replacement for Google Drive by periodically syncing with
+    Nextcloud instead.
+"""
+
+# python libraries
+import os
+import re # to parse cell names
+import string
+
+# third-party dependencies
+import collections   # for OrderedDict
+import getpass       # for getting the nextcloud user's password securely
+import pyexcel_ods   # for using an open spreadsheet format
+#import pyexcel_odsr # for pagination: https://github.com/pyexcel/pyexcel-odsr
+
+from nextcloud import NextCloud
+
+"""
+    A class to wrap any and all modifications to the spreadsheet files...
+    Based on the original implementation that used gspread, this new version
+    is intended to be free of Google services and instead process ODS files
+    "locally," wherever the bot script is running.
+
+    Let `book` be a wrapper of many `sheets`, and `sheets` be the typical
+    definition of a single table of data with a given name.
+"""
+class SpreadSheet:
+  def __init__(self, nextcld, **kwargs):
+    print("[DEBUG]    Constructor is being run")
+    if (nextcld != None):
+      print("[     ]        nextcloud sync is set ON")
+      self.enable_sync = True
+      self.nc_obj = nextcld
+      #self.nc_host = kwargs["nc_url"]
+      #self.nc_user = kwargs["nc_user"]
+      #self.nc_pass = kwargs["nc_pass"]
+    else:
+      print("[     ]        nextcloud sync is set OFF")
+      self.nc_obj = None
+      self.enable_sync = False
+
+  """
+      If enabled, this object can SYNC DATA with a remote nextcloud server
+  """
+  def sync():
+    """
+        TODO we also want to download the file to manage changes... right?
+        FIXME there'd be race conditions if multiple users try to edit and
+        download the same ODS file at the same time... how should we deal with
+        this? Add mutexes, or split it up into multiple ODS files, one per user,
+        and perhaps add a system of user permissions to each file?
+    """
+    upload_file(self.book_filename)
+  
+  # low-level nextcloud-specific functions
+  def upload_file(file_name, file_content=None, timestamp=None):
+    file_local_path = os.path.join(os.getcwd(), file_name)
+    
+    # In order to upload the file, create it right here and now!
+    # TODO replace with a `tempfile` invocation,
+    # in order to prevent overwriting a local file.
+    if (file_content is None):
+      f = open(file_name, "r")
+    else:
+      f = open(file_name, "w")
+      f.write(str(file_content))
+    
+    file_local_path = os.path.abspath(file_name)
+    print("About to try to upload \"%s\" to %s" % (file_content, file_name))
+    try:
+        self.nc_obj.upload_file(user_username, file_local_path, file_name, timestamp)
+    except Error:
+        print("Upload encountered a failure!!")
+    print("Just tried to upload...")
+    
+    # check status code -- TODO port this functionality somehow?
+    #assert res.is_ok
+    #assert res.raw.status_code == self.CREATED_CODE
+    
+    # test uploaded file can be found with list_folders
+    file_nextcloud_href = os.path.join(WebDAV.API_URL, user_username, file_name)
+    folder_info = self.nc_obj.list_folders(user_username, path=file_name)
+    assert folder_info.is_ok
+    assert len(folder_info.data) == 1
+    assert isinstance(folder_info.data[0], dict)
+    
+    # check href
+    assert folder_info.data[0]['href'] == file_nextcloud_href
+    
+    # remove file on local machine
+    os.remove(file_local_path)
+    self.nc_obj.download_file(user_username, file_name)
+    
+    # test file is downloaded to current dir
+    assert file_name in os.listdir(".")
+    f = open(file_local_path)
+    downloaded_file_content = f.read()
+    assert downloaded_file_content == file_content
+
+  # renamed from new_sheet to new_book, in the interest of clarity
+  def new_book(self, assigned_name):
+    self.book_filename = assigned_name
+    self.book = collections.OrderedDict()
+    new_worksheet("default")
+    if (assigned_name == None or assigned_name == ""):
+      pyexcel_ods.save_data("dataSheet.ods", self.book)
+    else:
+      pyexcel_ods.save_data(assigned_name, self.book)
+
+  # renamed from get_sheet to get_book, in the interest of clarity
+  def get_book(self, filename):
+    try:
+      self.book = pyexcel_ods.get_data(filename)
+    except FileNotFoundError:
+      self.new_book(filename)
+
+  # This call creates a new 2d array (for a new sheet inside the spreadsheet
+  # book), also allowing the user to specify a custom size of the array.
+  def new_worksheet(self, name, row_ct=2500, col_ct=50):
+    self.worksheet_name = name
+    sheet_as_arr = [[]]
+    for i in range(0, row_ct):
+        sheet_as_arr.append([])
+        for h in range(0, col_ct):
+            sheet_as_arr[i].append('')
+
+
+    sheet_as_dict = { name: sheet_as_arr }
+    
+    self.book.update(sheet_as_dict)
+    return self.book[name]
+
+  def get_worksheet(self, name, force):
+    self.worksheet_name = name
+    try:
+      return self.book[name]
+    except KeyError:
+      if (force):
+        # the `self.` here is necessary...? TODO find out why.
+        return self.new_worksheet(name)
+      else:
+        return None
+
+  def get_row(self, row, wsheet=None):
+    if (wsheet == None):
+      return self.book[self.worksheet_name][row]
+    else:
+      return self.book[wsheet][row]
+
+  # FIXME add logic to avoid OutOfBounds exceptions!
+  def get_column(self, column):
+    matrix = self.book[self.worksheet_name]
+    return [matrix[row][column] for row in self.book]
+  
+  """
+      GSpread actually offers a `Cell` object that wraps its value, row, and
+      column, in addition to its (letter,number) location (aka "cell label")...
+
+      I'm hoping we don't need to define out own cell object, but it is worth
+      noting...
+
+      We could of course parse out the label using RegEx, but then there's the
+      matter of actually converting the letter sequence to its index.
+
+      here, I make space for an algorithm to do this.
+  """
+  def convert_alphabetic_to_column(self, letter_seq):
+    sigma = 0
+    index = len(letter_seq) - 1
+    while (index >= 0):
+      factor = 1
+      count = 0
+      upper_limit = len(letter_seq) - index - 1
+      
+      # get the proper power of 26 to apply.
+      # Yes, I know I could use math.pow(), but I am afraid of doubles.
+      # Mock me if you want, but integers can grow as large as they need to in
+      # python, so there's no risk of imprecision or overflow.
+      while (count < upper_limit):
+        factor *= 26
+        count += 1
+      sigma += (string.ascii_uppercase.find(letter_seq[index].upper()))*factor
+      index -= 1
+    return sigma
+
+  def get_cell(self, cell_label):
+    match_obj = re.match("^(\\w+)(\\d+)$", cell)
+    assert(match_obj != None),"Invalid cell name. It must be a sequence of letters followed by a number."
+    row_num = int(match_obj.group(2))
+    column_num = self.convert_alphabetic_to_column(match_obj.group(1))
+    return self.book[worksheetname][row_num][column_num]
+
+  def write_cell(self, cell, message):
+    assert (len(cell) >= 2),"Invalid cell size. It must be at least two characters in length."
+    
+    # RECALL: Valid cell names could be really long like "ACE6561"
+    match_obj = re.match("^(\\w+)(\\d+)$", cell)
+    assert(match_obj != None),"Invalid cell name. It must be a sequence of letters followed by a number."
+    
+    row = int(match_obj.group(2)) - 1 # indices start at zero
+    col = self.convert_alphabetic_to_column(match_obj.group(1)) # index conversion built-in to the member function
+    
+    print("[DEBUG]    Now OVERWRITING value at row %d, col %d" % (row, col))
+    self.book[self.worksheet_name][row][col] = message
+    pyexcel_ods.save_data("ok.ods", self.book)
+
+    if (self.enable_sync):
+      print("connection to Nextcloud is a WIP")
+
+  def testGetMethods(self):
+    self.get_book("dataBook.ods")
+    self.get_worksheet('CINCO_DE_MAYO', True)
+    self.write_cell('A1', 'bruh zone')
+
+# TODO put simpler unit tests in a new file
+if __name__ == '__main__':
+  # nextcloud-related variables
+  #try:
+  #  NEXTCLOUD_URL = "http://{}:80".format(os.environ.get('NEXTCLOUD_HOSTNAME', 'localhost'))
+  #except KeyError:
+  
+  NEXTCLOUD_URL = input("enter the full nextcloud server url:")
+  # DO NOT HARD_CODE PASSWORDS IN PLAINTEXT!
+  user_username = input("Enter your username: ")
+  user_password = getpass.getpass("Enter your password: ")
+  
+  nxc_obj = NextCloud(NEXTCLOUD_URL, user_username, user_password, json_output=True)
+  
+  spreadSheet = SpreadSheet(nxc_obj)
+  spreadSheet.testGetMethods()
+
+
---
+++ new-main.py
@@ -0,0 +1,290 @@
+#todo list
+'''
+1. test out having multiple headers of the smae name (low priority)
+2. add data sorting and analysis (high priority)
+3. add time (medium priority)
+'''
+
+import getpass
+import os
+import string
+
+import gspreadtest_turned_ods_nc_test
+
+import spreadsh_mgr
+from discord.ext import commands
+import datetime
+import asyncio
+import nest_asyncio
+import gspread
+import discord
+import pandas
+import secret
+
+spreadsheet = spreadsh_mgr.SpreadSheet()
+
+"""
+    The actual python bot to manage the creation of spreadsheets and optional
+    management of message metadata.
+"""
+class dataBot:
+  def __init__(self):
+    @bot.event
+    async def on_ready():
+      print(f'We have logged in as {bot.user}')
+    spreadsheet.get_book()
+
+  def author_id_worksheet_not_found(self, authorID):
+    sheetList = spreadsheet.book.keys()
+    return (str(authorID) in sheetList)
+#    try:
+#      spreadsheet.get_worksheet(str(authorID))
+#      self.worksheet = spreadsheet.worksheet
+#      return False
+#    except gspread.WorksheetNotFound:
+#      return True
+
+  def findColumn(self, headers):
+    rowValues = spreadsheet.get_row(1)
+    count = 0
+    self.test = False
+
+    for i in rowValues:
+      if i == headers:
+        self.test = True
+        
+        # TODO find a better (non-overloaded) name for this instance variable!!
+        self.count = count
+      count += 1
+
+  def commands(self):
+    @bot.command()
+    async def ping(message):
+      await message.channel.send('Pong!')
+
+    @bot.command()
+    async def register(message):
+      try:
+        spreadsheet.new_worksheet(f'{message.author.id}')
+        await message.channel.send(f'worksheet made!')
+          
+      except gspread.exceptions.APIError:
+        await message.channel.send(f'Spreadsheet by the name of <@{message.author.id}> already exists')
+
+    @bot.command()
+    async def newHeader(message, *args):
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        #gets all cell values of top row
+        rowValues = spreadsheet.get_row(1)
+        count = 0
+        headers = ''
+        
+        #compiling arguments in message contents
+        headers = " ".join(str(next_header) for next_header in args)
+        
+        #find every second column so that I can fit datetine values for each cell (B, D, F, etc are all data columns while the other odds contain dates)
+        for i in rowValues:
+          count += 1
+        
+        #catching for odd numbers and gets converted to even (i think)
+        if type(count/2) == float:
+          count += 1
+          
+        #same title column values cause issues I dont want to build around
+        if headers in rowValues:
+          await message.channel.send(f'You can\'t make two headers of the same name. \n(if ur trying to find bugs id like you to know ur an asshole)')
+          
+        else:
+          spreadsheet.write_cell(f'{string.ascii_uppercase[count]}1', headers)
+        
+          await message.channel.send(f'Making header:`"{headers}"` on `{string.ascii_uppercase[count]}1` in file <@{message.author.id}>')
+
+
+    @bot.command()
+    async def displayHeaders(message): 
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        rowValues = spreadsheet.get_row(1)
+        cleaned = []
+        reformat = '['
+        
+        for i in rowValues:
+          if i != '':
+            cleaned.append(i)
+        
+        for i in cleaned:
+          #saving last value in list to prevent it ending in a comma
+          if cleaned[-1] != i:
+            reformat += f'`{i}`, '
+          
+        reformat += f'`{i}`]'
+        bindEmbed = discord.Embed(color=0xFF99E5)
+        bindEmbed.add_field(name='List of headers:', value=f'{reformat}', inline=True)
+        
+        await message.channel.send(embed=bindEmbed)
+        
+    @bot.command()
+    async def postData(message, *args):
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        #holy fucking shit this may work but its slow as fuck fix it when u got time
+        headers = ''
+        invalid = [len(args)-1, len(args)-2]
+        for i in range(len(args)):
+          if i not in invalid:
+            headers += args[i] + ' '
+        headers += args[-2]
+
+        self.findColumn(headers)
+        if self.test != True:
+          await message.channel.send(f'There is no header called `{headers}`. Try ;displayHeaders to see what headers you have')
+        else:
+          count = 2
+          while True:
+            cell = string.ascii_uppercase[self.count] + str(count)
+            datecell = string.ascii_uppercase[self.count-1] + str(count)
+
+            cellValue = spreadsheet.get_cell(cell)
+            if cellValue == '':
+              now = datetime.datetime.now()
+              formatted = now.strftime("%d/%m/%Y %H:%M:%S")
+              
+              spreadsheet.write_cell(datecell, formatted)
+              spreadsheet.write_cell(cell, args[-1])
+              bindEmbed = discord.Embed(color=0xFF99E5)
+              bindEmbed.set_author(name=f'Header: `{headers}`')
+              bindEmbed.add_field(name=f'Cell Value: `{cell}`', value=f'Data: `{args[-1]}`', inline=True)
+              await message.channel.send(embed=bindEmbed)
+              break
+
+            count += 1
+
+    @bot.command()
+    async def rawData(message, *args):
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        headers = ''
+        for i in args:
+          if i != args[-1]:
+            headers += i + ' '
+        headers += args[-1]
+
+        self.findColumn(headers)
+        if self.test != True:
+            await message.channel.send(f'There is no header called `{headers}`. Try ;displayHeaders to see what headers you have')
+        else:
+          columnValue = spreadsheet.get_column(self.count+1)
+          totalString = ''
+          cell = string.ascii_uppercase[self.count] + '2'
+          for i in range(len(columnValue)):
+            if i != 0 or len(columnValue)-1:
+              
+              totalString += f'(`{cell}`): ' + columnValue[i] + '\n'
+              cell = string.ascii_uppercase[self.count] + f'{i+2}'
+          totalString += f'(`{cell}`): ' + columnValue[-1]
+
+          bindEmbed = discord.Embed(color=0xFF99E5)
+          bindEmbed.set_author(name=f'Getting values of {headers}')
+          bindEmbed.add_field(name=f'Column: {headers}', value=f'{totalString}', inline=True)
+          bindEmbed.set_footer(text=f'Note that cells begin at {string.ascii_uppercase[self.count]}2, because the first cell is occupied by the header (title)')
+          await message.channel.send(embed=bindEmbed)
+
+    
+    @bot.command()
+    async def rawSheet(message):
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        rows = spreadsheet.get_row(1)
+        
+        bindEmbed = discord.Embed(color=0xFF99E5)
+        bindEmbed.set_author(name=f'Getting values of <@{message.author.id}>')
+        
+        for header in rows:
+          if header != '':
+            column = spreadsheet.get_column(header)
+            print(column)
+        '''
+        for header in rows:
+          column = spreadsheet.get_column(count)
+          for value in column:
+            cell = string.ascii_uppercase[count-1] + str(cellCount)
+            output += f'`{cell}`: {value}\n'
+            cellCount += 1
+          cellCount = 1
+          count += 1
+          bindEmbed.add_field(name=f'Column: {header}', value=f'{output}', inline=True)
+          output = ''
+        '''
+        
+        await message.channel.send(embed=bindEmbed)
+        
+    @bot.command()
+    async def replace(message, *args):
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        temp = ''
+        for i in range(len(args)):
+          if i != 0:
+            temp += args[i]
+            
+        try:
+          if args[0][1] != '1':
+            spreadsheet.write_cell(args[0], temp)
+            await message.channel.send(f'replaced `{args[0]}` with `{temp}`')
+          else:
+            await message.channel.send('you cant replace headers')
+        except:
+          await message.channel.send('something went wrong?')
+          
+
+    @bot.command()
+    async def basicAnalysis(message):
+      if self.author_id_worksheet_not_found(message.author.id):
+        await message.channel.send('you need to register a spreadsheet with ;register first')
+      else:
+        strdata = self.worksheet.get_all_values()
+        headers = strdata.pop(0)
+
+        intdata = []
+        for value in strdata:
+          tempList = []
+          for i in value:
+            try:
+              i = int(i)
+              tempList.append(i)
+            except:
+              pass
+          intdata.append(tempList)
+        
+        intdf = pandas.DataFrame(intdata, columns=headers)
+        strdf = pandas.DataFrame(strdata, columns=headers)
+        
+        
+        await message.channel.send(strdf)
+        await message.channel.send(intdf.describe())
+        await message.channel.send(intdf.info)
+        
+    
+    
+  def run(self):
+    self.commands()
+    bot.run(secret.credentials.bottoken)
+
+
+if __name__ == '__main__':
+  """
+      When the bot is first run, determine whether we need to obtain credentials
+      for the nextcloud server.
+  """
+  bot = commands.Bot(command_prefix=';')
+  nest_asyncio.apply()
+  dataBot = dataBot()
+  dataBot.run()
+
